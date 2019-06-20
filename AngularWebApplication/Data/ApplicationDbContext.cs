af1aee6615fc7d3b3c84f5b3244aaae6997456a2
Dalton <phandb72@gmail.com>
2019-06-19 22:38:06 -0500

    Add Angular, build database, perform migration

---
+++ AngularWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AngularWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AngularWebApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Constructor to set up connection string
+        public ApplicationDbContext() : base("AppConnectionString") { }
+
+        public static ApplicationDbContext Create() => new ApplicationDbContext();
+
+        //add data set for model to fetch, update, or delete record from table
+        public DbSet<PlayerRecord> PlayerRecords { get; set; }
+    }
+}
---
+++ AngularWebApplication/Models/PlayerRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AngularWebApplication.Models
+{
+    public class PlayerRecord
+    {
+        [Key]
+        public int PlayerId { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public string Club { get; set; }
+
+        public string Position { get; set; }
+
+        public DateTime JoinedClubOn { get; set; }
+    }
+}
